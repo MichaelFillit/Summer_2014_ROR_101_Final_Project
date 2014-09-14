d4902f1290d8eaa2513b8c72c004316154129f04
Michael Fillit <fillitmichael@gmail.com>
2014-09-14 17:51:14 -0400

    update mac

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828132509) do
+ActiveRecord::Schema.define(version: 20140904045157) do
 
   create_table "dashboards", force: true do |t|
     t.string   "title"
